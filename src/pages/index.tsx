2c611e63855e89c27275a9da799c23c4cff0e204
Daniel Kadar <kdrdani@gmail.com>
2023-04-07 15:00:00 +0200

    get random meal in homepage, added type for meal from api response

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import Head from "next/head";
+import useFetch from '@/hooks/useFetch';
+import { Meal } from "@/types/meal";
 
 export default function Home() {
+  const { data, loading, error } = useFetch<Meal[]>('https://themealdb.com/api/json/v1/1/random.php');
+
+  if (loading) {
+    /** @todo loader component */
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    /** @todo error component */
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +24,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={''}>
-        <p>HELLO</p>
+        {/** @todo implement container that shows a random meal */}
+        <div>{JSON.stringify(data, null, 2)}</div>
       </main>
     </>
   );
