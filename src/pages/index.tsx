1b9127977debf6e1d8457f9929373004b8b473d8
Daniel Kadar <kdrdani@gmail.com>
2023-04-07 16:22:20 +0200

    created a dynamic route for the dish detail page, use MealCard component in both home and detail pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
-import useFetch from '@/hooks/useFetch';
-import { Meal } from "@/types/meal";
+import useFetch from "@/hooks/useFetch";
+import { Meals } from "@/types/meal";
+import MealCard from "@/components/ui/mealCard/MealCard";
+import Link from "next/link";
+
+const NoResult = () => {
+  return <p>No Result</p>;
+};
 
 export default function Home() {
-  const { data, loading, error } = useFetch<Meal[]>('https://themealdb.com/api/json/v1/1/random.php');
+  const { data, loading, error } = useFetch<Meals>(
+    "https://themealdb.com/api/json/v1/1/random.php"
+  );
 
   if (loading) {
     /** @todo loader component */
@@ -12,9 +20,14 @@ export default function Home() {
 
   if (error) {
     /** @todo error component */
-    return <p>Error: {error.message}</p>;
+    return <p>{error.message}</p>;
   }
 
+  if (!data || !data.meals.length) {
+    return <NoResult />;
+  }
+
+  const meal = data.meals[0];
   return (
     <>
       <Head>
@@ -23,9 +36,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={''}>
+      {/** @todo get loading and error state in here for be inside of main */}
+      <main className="">
         {/** @todo implement container that shows a random meal */}
-        <div>{JSON.stringify(data, null, 2)}</div>
+        <div className="">
+          <MealCard meal={meal} />
+          <Link href={`/meal/${meal.idMeal}`}>
+            Details
+          </Link>
+        </div>
       </main>
     </>
   );
---
+++ src/pages/meal/[id].tsx
@@ -0,0 +1,43 @@
+import MealCard from "@/components/ui/mealCard/MealCard";
+import useFetch from "@/hooks/useFetch";
+import { Meals } from "@/types/meal";
+import Head from "next/head";
+import { useRouter } from 'next/router'
+
+export default function Meal() {
+  const router = useRouter()
+  const { id } = router.query;
+
+  const { data, loading, error } = useFetch<Meals>(
+    `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+
+  if (loading) {
+    /** @todo loader component */
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    /** @todo error component */
+    return <p>{error.message}</p>;
+  }
+
+  if (!data || !data.meals.length) {
+    return <p>Meal not found</p>;
+  }
+
+  const meal = data.meals[0];
+  return (
+    <>
+      <Head>
+        <title>Dish detail</title>
+        <meta name="description" content="Dish detail" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={""}>
+        <MealCard meal={meal} />
+      </main>
+    </>
+  );
+}
