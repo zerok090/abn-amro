7be382f3527c778e2877ddf91fea6054e6a9344e
Daniel Kadar <kdrdani@gmail.com>
2023-04-07 17:16:15 +0200

    added logo for header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,19 +1,15 @@
+import Image from "next/image";
 import React from "react";
-import Link from "next/link";
 
 const Header = () => {
   return (
     <header>
-      <div className="icon"></div>
+      <div className="logo">
+        <Image src='/logo.png' width={200} height={150} alt='Logo' /> 
+      </div>
       <div className="search"></div>
-      <nav>
-        <ul className="navbar">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
+
 export default Header;
