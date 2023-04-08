46ac73369e4ab680af73d15ede0ea61688167bdb
Daniel Kadar <kdrdani@gmail.com>
2023-04-08 21:35:09 +0200

    pages updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import MealRandom from "@/components/ui/mealRandom/MealRandom";
 import Head from "next/head";
+import MealRandom from "@/components/ui/mealRandom/MealRandom";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Dishes</title>
+        <title>Homepage - Random Dish</title>
         <meta name="description" content="Dish home" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
--- src/pages/meals/[query].tsx
+++ src/pages/meals/[query].tsx
@@ -1,15 +1,15 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
 import MealCard from "@/components/ui/mealCard/MealCard";
 import useFetch from "@/hooks/useFetch";
 import type { Meals } from "@/types/meal";
-import Head from "next/head";
-import { useRouter } from "next/router";
 
 export default function Meal() {
   const router = useRouter();
-  const { id } = router.query;
+  const { query } = router.query;
 
   const { data, loading, error } = useFetch<Meals>(
-    `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    `https://themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
 
   if (loading) {
@@ -23,20 +23,21 @@ export default function Meal() {
   }
 
   if (!data || !data.meals || !data.meals.length) {
-    return <p>Meal not found</p>;
+    return <p>No meals found</p>;
   }
 
-  const meal = data.meals[0];
   return (
     <>
       <Head>
-        <title>{meal.strMeal}</title>
+        <title>Meals</title>
         <meta name="description" content="Dish detail" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={""}>
-        <MealCard meal={meal} />
+        {data.meals.map((meal) => {
+            return <MealCard key={meal.idMeal} meal={meal} />
+        })}
       </main>
     </>
   );
