d958329852492a6a1db9a5610f560dbd4ceea721
Daniel Kadar <kdrdani@gmail.com>
2023-04-08 21:33:01 +0200

    addition of search icon, search is its own component now

---
+++ src/components/common/search/Search.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import styles from "./Search.module.scss";
+import SearchIcon from "@/../public/icons/search-icon.svg";
+
+interface SearchProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: React.FormEvent) => void;
+}
+
+function Search({ value, onChange, onSubmit }: SearchProps) {
+  const handleOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(e);
+  }
+
+  return (
+    <form className={styles.searchContainer} onSubmit={handleOnSubmit}>
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder="Search"
+        onChange={onChange}
+        value={value}
+      />
+      <button className={styles.searchButton} type="submit">
+        <SearchIcon className={styles.icon} />
+      </button>
+    </form>
+  );
+}
+
+export default Search;
