e134295c2f5b83c443b92b376439d3dafc5e2cec
Daniel Kadar <kdrdani@gmail.com>
2023-04-09 22:20:01 +0200

    addition of ingredients and instructions to meal detail page

--- src/pages/meal/[id].tsx
+++ src/pages/meal/[id].tsx
@@ -1,8 +1,26 @@
 import MealCard from "@/components/ui/mealCard/MealCard";
 import useFetch from "@/hooks/useFetch";
-import type { Meals } from "@/types/meal";
+import type { Meals, Meal as TMeal, KeyMeal } from "@/types/meal";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import styles from "./Meal.module.scss";
+
+function getIngredientsArray(meal: TMeal) {
+  const keysMeal = Object.keys(meal) as Array<KeyMeal>;
+  return keysMeal.reduce((result, currentKey) => {
+    if (!meal[currentKey] || meal[currentKey] === "") return result;
+    if (!currentKey.includes("strIngredient")) return result;
+
+    const index = currentKey.substring(13);
+    const measurementKey = `strMeasure${index}` as KeyMeal;
+
+    const ingredient = meal[currentKey];
+    const measurment = meal[measurementKey];
+
+    result.push(`${measurment} ${ingredient}`);
+    return result;
+  }, [] as string[]);
+}
 
 export default function Meal() {
   const router = useRouter();
@@ -28,8 +46,7 @@ export default function Meal() {
 
   const meal = data.meals[0];
   const videoId = meal.strYoutube.match(/watch\?v=(.*)/);
-  const ingredients = ["1"];
-  console.log(meal);
+  const ingredientsArray = getIngredientsArray(meal);
 
   return (
     <>
@@ -39,19 +56,29 @@ export default function Meal() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={""}>
-        <MealCard meal={meal} toDetail={false} />
-        {/** @todo: list of ingredients including measures, instructions */}
-        {/** @todo get this into its own component */}
-        {videoId !== null && videoId[1] ? (
-          <iframe
-            width="560"
-            height="315"
-            src={`https://youtube.com/embed/${videoId[1]}`}
-          ></iframe>
+      <main className={styles.container}>
+        <section>
+          <MealCard meal={meal} toDetail={false} />
+        </section>
+        {videoId && videoId[1] ? (
+          <section>
+            <iframe
+              width="560"
+              height="315"
+              src={`https://youtube.com/embed/${videoId[1]}`}
+            ></iframe>
+          </section>
         ) : null}
-        <ul>{ingredients.map((ingredient) => null)}</ul>
-        <details></details>
+        <section>
+          <ul>
+            {ingredientsArray.map((ingredient, index) => {
+              return <li key={index}>{ingredient}</li>;
+            })}
+          </ul>
+        </section>
+        <section>
+          <p>{meal.strInstructions}</p>
+        </section>
       </main>
     </>
   );
