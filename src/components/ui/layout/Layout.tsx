709c8ee42802aedca0e280fc9310cc685a645e2a
Daniel Kadar <kdrdani@gmail.com>
2023-04-09 17:39:04 +0200

    layout container + scss

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import React, { PropsWithChildren } from "react";
 import Header from "../header/Header";
+import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
-      {children}
-    </>
+      <main className={styles.mainContainer}>{children}</main>
+      <footer></footer>
+    </div>
   );
 };
 export default Layout;
