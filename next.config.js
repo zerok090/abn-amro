0a735800ebd4a786818aa517a3025b52eb549119
Daniel Kadar <kdrdani@gmail.com>
2023-04-09 17:38:12 +0200

    formatting in config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
+      rule.test?.test?.(".svg")
+    );
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
@@ -18,19 +20,19 @@ const nextConfig = {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    )
+        use: ["@svgr/webpack"],
+      }
+    );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
+    fileLoaderRule.exclude = /\.svg$/i;
 
-    return config
+    return config;
   },
   reactStrictMode: true,
   images: {
-    domains: ['www.themealdb.com'],
-},
-}
+    domains: ["www.themealdb.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
