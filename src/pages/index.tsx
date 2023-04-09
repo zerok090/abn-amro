5376929861545931f424dcd6ad422aa52f1cd092
Daniel Kadar <kdrdani@gmail.com>
2023-04-09 17:39:48 +0200

    container change homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import MealRandom from "@/components/ui/mealRandom/MealRandom";
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
@@ -11,11 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.mainContainer}>
-        <section className={styles.mealRandomContainer}>
-          <MealRandom />
-        </section>
-      </main>
+
+      <section className={styles.mealRandomContainer}>
+        <MealRandom />
+      </section>
     </>
   );
 }
